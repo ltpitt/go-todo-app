ba6fc68f3e95ecb491bacbb9c3f2b81eb1a6b137
Davide Nastri <d.nastri@gmail.com>
2022-01-04 11:35:24 +0100

    feat: Added ReadItems in todo.go

--- todo/todo.go
+++ todo/todo.go
@@ -1,8 +1,8 @@
 package todo
 
 import (
-	"io/ioutil"
 	"encoding/json"
+	"io/ioutil"
 )
 
 type Item struct {
@@ -13,8 +13,20 @@ func SaveItems(filename string, items []Item) error {
 	b, err := json.Marshal(items)
 	err = ioutil.WriteFile(filename, b, 0644)
 	if err != nil {
-		return err	
+		return err
 	}
-	
+
 	return nil
+}
+
+func ReadItems(filename string) ([]Item, error) {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return []Item{}, nil
+	}
+	var items []Item
+	if err := json.Unmarshal(b, &items); err != nil {
+		return []Item{}, err
+	}
+	return items, nil
 }
