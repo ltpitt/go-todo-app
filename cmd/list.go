e05ee2a97db3a93745a919b6603b8828faeb15df
Davide Nastri <d.nastri@gmail.com>
2022-01-10 14:36:36 +0100

    feat: Added list command

--- cmd/list.go
+++ cmd/list.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,43 +17,41 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/ltpitt/go-todo-app/todo"
 	"github.com/spf13/cobra"
 )
 
-// addCmd represents the add command
-var addCmd = &cobra.Command{
-	Use:   "add",
-	Short: "Add a new todo",
-	Long:  `Add will create a new todo item to the list`,
-	Run:   addRun,
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		items, err := todo.ReadItems("todo.json")
+		if err != nil {
+			log.Printf("%v", err)
+		}
+		fmt.Println(items)
+	},
 }
 
 func init() {
-	rootCmd.AddCommand(addCmd)
+	rootCmd.AddCommand(listCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-func addRun(cmd *cobra.Command, args []string) {
-	items := []todo.Item{}
-	for _, x := range args {
-		items = append(items, todo.Item{Text: x})
-	}
-
-	err := todo.SaveItems("todo.json", items)
-	if err != nil {
-		fmt.Errorf("%v", err)
-	}
-
-	todo.SaveItems("x", items)
+	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
