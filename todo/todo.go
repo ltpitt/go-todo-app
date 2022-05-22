a03e105ac6a7fa12490e5fd668aed05f1cd50bf1
Davide Nastri <d.nastri@gmail.com>
2022-05-22 23:49:31 +0200

    feat: Added Priority

--- todo/todo.go
+++ todo/todo.go
@@ -7,6 +7,18 @@ import (
 
 type Item struct {
 	Text string
+	Priority int
+}
+
+func (i *Item) SetPriority(pri int) {
+	switch pri {
+	case 1:
+		i.Priority = 1
+	case 3:
+		i.Priority = 3
+	default:
+		i.Priority = 2
+	}
 }
 
 func SaveItems(filename string, items []Item) error {
