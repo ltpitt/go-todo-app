6221cd1b1df3cdc1fc508e405cbde09d55860362
Davide Nastri <d.nastri@gmail.com>
2022-07-13 22:19:39 +0200

    feat: Implementing sort

--- todo/todo.go
+++ todo/todo.go
@@ -6,6 +6,20 @@ import (
 	"strconv"
 )
 
+type ByPri []Item
+
+func (s ByPri) Len() int {
+	return len(s)
+}
+
+func (s ByPri) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s ByPri) Less(i, j int) bool {
+	if s[i].Priority == s[j].Priority {
+		return s[i].position < s[j].position
+	}
+	return s[i].Priority < s[j].Priority
+}
+
 type Item struct {
 	Text     string
 	Priority int
